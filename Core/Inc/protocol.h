c5039f4409a575d967785c1fbaa422fc676e135b
Hang Li <393059457lh@gmail.com>
2024-12-27 18:02:46 +0800

    Add protocol

---
+++ Core/Inc/protocol.h
@@ -0,0 +1,26 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#include <cstdint>
+#include <list>
+#include <string>
+
+class Segment
+{
+public:
+    void Receive(const uint8_t *data, const uint32_t &len);
+    bool GetOne(std::string &str);
+
+public:
+    static void Pack(std::string &str, const uint8_t *cmd, const std::string &data);
+
+private:
+    static constexpr uint8_t mHeaders[2] = {0xac, 0x53};
+    static constexpr uint8_t mTrailers[2] = {0x35, 0xca};
+    std::list<std::string> mSegments;
+    std::string mBuffer;
+    uint8_t mState = 0;
+    unsigned int mTrailerBegin = 5;
+};
+
+#endif
---
+++ Core/Src/protocol.cpp
@@ -0,0 +1,85 @@
+#include "protocol.h"
+
+void Segment::Receive(const uint8_t *data, const uint32_t &len)
+{
+    mBuffer.append((char *)data, len);
+    unsigned int bufferLen = mBuffer.length();
+    unsigned int loop = bufferLen - 1;
+    if (bufferLen > 1 && mState == 0)
+    {
+        for (unsigned int i = 0; i < loop; i++)
+        {
+            if (mBuffer[i] == mHeaders[0] && mBuffer[i + 1] == mHeaders[1])
+            {
+                mBuffer.erase(0, i);
+                mState = 1;
+                break;
+            }
+        }
+        if (mState == 0)
+        {
+            mBuffer.erase(0, loop);
+        }
+    }
+    if (mState == 1)
+    {
+        while (mTrailerBegin < loop)
+        {
+            if (mBuffer[mTrailerBegin] == mTrailers[0] && mBuffer[mTrailerBegin + 1] == mTrailers[1])
+            {
+                uint8_t checksum = 0;
+                unsigned int checksumIndex = mTrailerBegin - 1;
+                for (unsigned int i = 2; i < checksumIndex; i++)
+                {
+                    checksum += mBuffer[i];
+                }
+                if (mBuffer[checksumIndex] == checksum)
+                {
+                    mSegments.push_front(std::string(mBuffer, 2, checksumIndex - 2));
+                }
+                mBuffer.erase(0, mTrailerBegin + 2);
+                mState = 0;
+                mTrailerBegin = 5;
+                break;
+            }
+            else
+            {
+                mTrailerBegin++;
+            }
+        }
+    }
+}
+
+bool Segment::GetOne(std::string &str)
+{
+    if (!mSegments.empty())
+    {
+        str = mSegments.back();
+        mSegments.pop_back();
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void Segment::Pack(std::string &str, const uint8_t *cmd, const std::string &data)
+{
+    uint8_t checksum;
+    str.resize(data.length() + 7);
+    str[0] = mHeaders[0];
+    str[1] = mHeaders[1];
+    str[2] = cmd[0];
+    str[3] = cmd[1];
+    checksum = str[2] + str[3];
+    unsigned int i = 4;
+    for (const auto &it : data)
+    {
+        str[i++] = it;
+        checksum += it;
+    }
+    str[data.length() + 4] = checksum;
+    str[data.length() + 5] = mTrailers[0];
+    str[data.length() + 6] = mTrailers[1];
+}
